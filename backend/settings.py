ef6b8a7759d10d8c9c145acb947f7e0b711100e2
Vitalii <vitaliikalyta96@gmail.com>
2022-08-27 18:05:49 +0300

    Prepared to deployment with heroku

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
